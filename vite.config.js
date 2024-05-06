189457c7c70f03368f823a98c1b76857a78c23b3
Jorge Alcario <jorgefca@gmail.com>
2024-05-06 11:21:04 +0100

    working on CI

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,18 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig(async({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
     var appenv = env.VITE_APP_ENV
     var baseurl = env.BASE_URL
+
+    // to bypass problem with gh workflow and npm/vite failing to correctly select ghpages env
+    // we manually override it here
+    if (appenv == "ghpages") {
+        baseurl ="gnmd-gui"
+    }
+
     console.log(`Building ENV: ${appenv} with base URL: ${baseurl}`)
     return {
         base: baseurl,
